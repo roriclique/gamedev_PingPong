016fafa9a2f20b932e95eddb17892be722bd8171
md <dernpvyh.m@gmail.com>
2024-06-17 15:37:12 +0400

    Fix: updata 1.1

--- PingPong_project/Assets/NewAssets/Scripts/Controller/SecondPlayerController.cs
+++ PingPong_project/Assets/NewAssets/Scripts/Controller/SecondPlayerController.cs
@@ -13,7 +13,7 @@ public class SecondPlayerController : MonoBehaviour
     public void Player2Controller()
     {
         float moveInput = Input.GetAxis("Vertical");
-        float newY = transform.position.y + moveInput * (ball.speed * 2) * Time.deltaTime;
+        float newY = transform.position.y + moveInput * (ball.speed * 5) * Time.deltaTime;
         float clampedY = Mathf.Clamp(newY, -fieldWidthUnits / 2, fieldWidthUnits / 2);
 
         transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
