51a3acb00f02d6a6a96e609c6ddc8f9b4a1beda5
md <dernpvyh.m@gmail.com>
2024-07-09 12:12:48 +0400

    Update commit 1.5

--- PingPong_fixed/Assets/MyAssets/Scripts/Ball/Ball.cs
+++ PingPong_fixed/Assets/MyAssets/Scripts/Ball/Ball.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
+
 public class Ball : MonoBehaviour
 {
-    [HideInInspector] public float speed = 13f;
+    [HideInInspector] public float speed = 5f;
 
     private float accelSpeed = 0.2f;
     private Vector2 direction;
     private Vector2 startPosition;
     private float startSpeed;
     public Rigidbody2D rb;
-
-    
 
     void Start()
     {
@@ -30,24 +29,38 @@ public class Ball : MonoBehaviour
         Vector2 normal = collision.contacts[0].normal;
         float newPos = direction.x * normal.x + direction.y * normal.y;
         
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             direction = direction - 2 * newPos * normal;
             speed += accelSpeed;
+
+            Debug.Log("Direction after Player: " + direction);
         }
 
-        if (collision.gameObject.CompareTag("Shape"))
-        {
-            direction = direction - 2 * newPos * normal;
-        }
-
-        if (collision.gameObject.CompareTag("GameZone"))
+        else if (collision.gameObject.CompareTag("Player2"))
         {
             direction = direction - 2 * newPos * normal;
             speed += accelSpeed;
+
+            Debug.Log("Direction after Player2: " + direction);
         }
 
-        if (collision.gameObject.CompareTag("Score1Player") || collision.gameObject.CompareTag("Score2Player"))
+        else if (collision.gameObject.CompareTag("Shape"))
+        {
+            direction = direction - 2 * newPos * normal;
+            
+            Debug.Log("Direction after Shape: " + direction);
+        }
+
+        else if (collision.gameObject.CompareTag("GameZone"))
+        {
+            direction = direction - 2 * newPos * normal;
+            speed += accelSpeed;
+
+            Debug.Log("Direction after GameZone: " + direction);
+        }
+
+        else if (collision.gameObject.CompareTag("Score1Player") || collision.gameObject.CompareTag("Score2Player"))
         {
             transform.position = startPosition;
             direction = new Vector2(Random.Range(-3f, 3f), Random.Range(-0.5f, 0.5f));
